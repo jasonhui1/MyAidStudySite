76f22c9510d6d956d7fa0bad22b7a291273c358e
Jason Hui <jason903813@gmail.com@>
2023-03-10 12:24:19 +0800

    fix query

--- project_frontend/src/CMS/getdata.js
+++ project_frontend/src/CMS/getdata.js
@@ -1,17 +1,22 @@
 export const searchQuery = (searchTerm, keys) => {
 
-    if (keys.length===0){
-        keys=['title']
-    }    
+    if (keys.length === 0) {
+        keys = ['title']
+    }
 
     // Search through all words with space between them
-    const searchArray = searchTerm.split(' ')
-    // const keywordmap = searchArray.map(searchArray => `'${searchArray}' in keywords[]->word`).join('||')
-    // const keymap = searchArray.map(searchArray => ` ${searchKeyWord}`).join('&&')
-    // const allmap = searchArray.map(searchArray => `[${keys}] match '*${searchArray}*' || ${keywordmap}`).join('&&')
-    const allmap = searchArray.map(searchArray => `[${keys}] match '*${searchArray}*' || keywords[]->word match '*${searchArray}*'`).join('&&')
+    const searchArray = searchTerm.split(' ').filter(i => i)
+    let allmap = searchArray?.map(searchArray => `([${keys}] match '*${searchArray}*' || keywords[]->word match '*${searchArray}*')`).join('&&')
 
-    const query = `*[_type=='inspiration' && (${allmap} )]{
+    let query;
+
+    if (searchArray.length === 0){
+        query = `*[_type=='inspiration']`
+    } else {
+        query = `*[_type=='inspiration' && ${allmap}]`
+    }
+
+    query += `{ 
         _id,
           title,
           artist,
@@ -20,5 +25,17 @@ export const searchQuery = (searchTerm, keys) => {
           keywords[]->{_id, word}
           
       }`
+
+      console.log('query', query)
     return query
+
+    //Score system, not the best because sanity cant calculate score when matching with array
+    // *[_type=='inspiration'] | score(artist match "*late*" || title match '*volume*')    {
+    //     keywords[]->{word},
+    //        'keywordMatch': keywords[]->word match '*VFX*',
+    //         title,
+    //         artist,
+    //         _score,
+    //   } | order(keywordMatch desc) | order(_score desc) 
+    //   [ (_score > 0 || keywordMatch)]
 };
