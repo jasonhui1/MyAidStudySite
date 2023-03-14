83a8f1214a7ff1847827eb07b1d3814a8abd1d61
Jason Hui <jason903813@gmail.com>
2023-03-14 10:47:25 +0800

    Convert some pages to typescript

--- project_frontend/src/Pages/Breakdown.tsx
+++ project_frontend/src/Pages/Breakdown.tsx
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { client, urlFor } from '../client';
-import { PortableText } from '@portabletext/react'
+import { PortableText, PortableTextReactComponents } from '@portabletext/react'
 import { getBreakdownData } from '../FetchData/getdata';
 // import {getImageDimensions} from '@sanity/asset-utils'
 
+interface Article {
+    /**
+     * content of the block.
+     */
+    content: any;
+}
+
+
+
 export default function Breakdown() {
 
-    const [portableText, setPortableText] = useState()
+    const [portableText, setPortableText] = useState<JSX.Element>()
 
     useEffect(() => {
         client.fetch(getBreakdownData())
-            .then(article => {
+            .then((article: Article) => {
                 setPortableText(
                     <PortableText value={article.content} components={components} />,
                 )
@@ -27,23 +36,33 @@ export default function Breakdown() {
     )
 }
 
-const SampleVideoComponent = ({ value}) => {
+interface TypesComponent {
+    value: {
+        asset: {
+            _ref: string;
+        };
+        alt: string;
+    };
+    isInline: boolean
+}
+
+const SampleVideoComponent = ({ value }: TypesComponent): JSX.Element => {
     const [_file, id, extension] = value.asset._ref.split('-');
-    const PROJECT_ID= client.config().projectId
+    const PROJECT_ID = client.config().projectId
     const DATASET = client.config().dataset
 
     const url = `https://cdn.sanity.io/files/${PROJECT_ID}/${DATASET}/${id}.${extension}`
 
     return (
         <video controls>
-            <source src={url} type="video/mp4"/>
+            <source src={url} type="video/mp4" />
         </video>
     )
 
 }
 
 
-const SampleImageComponent = ({ value, isInline }) => {
+const SampleImageComponent = ({ value, isInline } : TypesComponent) : JSX.Element => {
     // console.log('value', value)
     return (
         <img
@@ -58,15 +77,15 @@ const SampleImageComponent = ({ value, isInline }) => {
     )
 }
 
-const components = {
+const components: Partial<PortableTextReactComponents> = {
     list: {
         // Ex. 1: customizing common list types
-        bullet: ({children}) => <ul className="mt-xl">{children}</ul>,
-        number: ({children}) => <ol className="mt-lg">{children}</ol>,
-    
+        bullet: ({ children }) => <ul className="mt-xl">{children}</ul>,
+        number: ({ children }) => <ol className="mt-lg">{children}</ol>,
+
         // Ex. 2: rendering custom lists
-        checkmarks: ({children}) => <ol className="m-auto text-lg">{children}</ol>,
-      },
+        checkmarks: ({ children }) => <ol className="m-auto text-lg">{children}</ol>,
+    },
     listItem: {
         bullet: ({ children }) => <li>{children}</li>,
         number: ({ children }) => <li>{children}</li>,
@@ -76,11 +95,11 @@ const components = {
     types: {
         image: SampleImageComponent,
         file: SampleVideoComponent,
-        code: props => (
-            <pre data-language={props.node.language}>
-                <code>{props.node.code}</code>
-            </pre>
-        )
+        // code: props => (
+        //     <pre data-language={props.node.language}>
+        //         <code>{props.node.code}</code>
+        //     </pre>
+        // )
     },
 
 }
